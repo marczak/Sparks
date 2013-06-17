fd8b2e4b4facf43c7140e187841df95fed958847
Edward Marczak <marczak@Abyss.local>
2013-06-17 09:44:30 -0400

    Initial Commit

---
+++ Sparks/SparksView.h
@@ -0,0 +1,13 @@
+//
+//  SparksView.h
+//  Sparks
+//
+//  Created by Edward Marczak on 6/17/13.
+//  Copyright (c) 2013 Radiotope. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface SparksView : ScreenSaverView
+
+@end
---
+++ Sparks/SparksView.m
@@ -0,0 +1,52 @@
+//
+//  SparksView.m
+//  Sparks
+//
+//  Created by Edward Marczak on 6/17/13.
+//  Copyright (c) 2013 Radiotope. All rights reserved.
+//
+
+#import "SparksView.h"
+
+@implementation SparksView
+
+- (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
